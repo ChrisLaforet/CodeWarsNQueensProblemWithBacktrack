03b4ce77800f6484cb793891450d6ac3dcf9c2eb
Chris Laforet <laforet@chrislaforetsoftware.com>
2021-08-11 21:34:36 -0400

    Added logic for calculating weights

--- n_queens.h
+++ n_queens.h
@@ -27,6 +27,40 @@ public:
     }
 };
 
+class square_weight {
+    int x;
+    int y;
+    bool hasQueen = false;
+    int weight = 0;
+
+public:
+    explicit square_weight(int x, int y, bool hasQueen) {
+        this->x = x;
+        this->y = y;
+        this->hasQueen = hasQueen;
+    }
+
+    int getX() {
+        return x;
+    }
+
+    int getY() {
+        return y;
+    }
+
+    bool isQueenSquare() {
+        return hasQueen;
+    }
+
+    void addWeight() {
+        ++weight;
+    }
+
+    int getWeight() {
+        return weight;
+    }
+};
+
 class board {
     int squaresPerSide = 0;
     bool** squares = nullptr;
@@ -47,6 +81,80 @@ class board {
             throw std::exception("out of range");
         }
         squares[y][x] = value;
+    }
+
+    bool leftPressureOn(int x, int y) {
+        if (x <= 0 || x >= squaresPerSide)
+            return false;
+        for (int xIndex = x - 1; x >= 0; x--) {
+            if (isQueenAt(xIndex, y)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool rightPressureOn(int x, int y) {
+        if (x < 0 || x < (squaresPerSide - 1))
+            return false;
+        for (int xIndex = x + 1; x < squaresPerSide; x++) {
+            if (isQueenAt(xIndex, y)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool topLeftPressureOn(int x, int y) {
+        if (x <= 0 || x >= squaresPerSide)
+            return false;
+        if (y <= 0 || y >= squaresPerSide)
+            return false;
+        for (int xIndex = x - 1, yIndex = y - 1; x >= 0, y >= 0; x--, y--) {
+            if (isQueenAt(xIndex, yIndex)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool bottomLeftPressureOn(int x, int y) {
+        if (x <= 0 || x >= squaresPerSide)
+            return false;
+        if (y < 0 || y < (squaresPerSide - 1))
+            return false;
+        for (int xIndex = x - 1, yIndex = y + 1; x >= 0, y < squaresPerSide; x--, y++) {
+            if (isQueenAt(xIndex, yIndex)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool topRightPressureOn(int x, int y) {
+        if (x <= 0 || x >= squaresPerSide)
+            return false;
+        if (y < 0 || y < (squaresPerSide - 1))
+            return false;
+        for (int xIndex = x + 1, yIndex = y + 1; x < squaresPerSide, y < squaresPerSide; x++, y++) {
+            if (isQueenAt(xIndex, yIndex)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool bottomRightPressureOn(int x, int y) {
+        if (x < 0 || x < (squaresPerSide - 1))
+            return false;
+        if (y < 0 || y < (squaresPerSide - 1))
+            return false;
+        for (int xIndex = x + 1, yIndex = y + 1; x < squaresPerSide, y < squaresPerSide; x++, y++) {
+            if (isQueenAt(xIndex, yIndex)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 public:
@@ -85,6 +193,10 @@ public:
         flipSquareAt(x, y, false);
     }
 
+    bool isQueenAt(int x, int y) {
+        return squares[y][x];
+    }
+
     std::string getPrintableRepresentation() {
         std::string result;
         for (int yIndex = 0; yIndex < squaresPerSide; yIndex++) {
@@ -94,6 +206,32 @@ public:
             result += "\n";
         }
         return result;
+    }
+
+    square_weight **getWeights(int x) {
+        square_weight **weights = new square_weight *[squaresPerSide];
+        for (int yIndex = 0; yIndex < squaresPerSide; yIndex++) {
+            weights[yIndex] = new square_weight(x, yIndex, isQueenAt(x, yIndex));
+            if (leftPressureOn(x, yIndex)) {
+                weights[yIndex]->addWeight();
+            }
+            if (rightPressureOn(x, yIndex)) {
+                weights[yIndex]->addWeight();
+            }
+            if (topLeftPressureOn(x, yIndex)) {
+                weights[yIndex]->addWeight();
+            }
+            if (bottomLeftPressureOn(x, yIndex)) {
+                weights[yIndex]->addWeight();
+            }
+            if (topRightPressureOn(x, yIndex)) {
+                weights[yIndex]->addWeight();
+            }
+            if (bottomRightPressureOn(x, yIndex)) {
+                weights[yIndex]->addWeight();
+            }
+        }
+        return weights;
     }
 };
 
