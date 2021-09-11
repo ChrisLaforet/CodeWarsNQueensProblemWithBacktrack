90d3a5a27c3665aa8ef67640518b598f4693912e
Chris Laforet <Chris.Laforet@shuttereleasefilms.com>
2021-09-10 22:29:07 -0400

    Working through memory errors

--- n_queens.h
+++ n_queens.h
@@ -105,12 +105,11 @@ inline bool checkSquareIsAvailable(const std::list<square *>& queenSquares, int 
 //int totalRecursions = 0;
 
 bool solveColumn(int squaresPerSide, std::list<square *>& queenSquares,
-                 const std::vector<int>& openColumns) {
+                 const int *openColumns, int openColumnCount) {
 //++totalRecursions;
 
     std::list<solveColumnData *> solveStack;
     auto *columnData = new solveColumnData(0, openColumns[0], squaresPerSide);
-
     bool isSuccess = false;
     do {
         do {
@@ -121,9 +120,11 @@ for (int count = 0; count < solveStack.size(); count++) {
 }
 std::cout<<nextColumnOffset<<" ("<<queenSquares.size()<< ")"<<std::endl<<std::flush;
                 columnData->setQueenSquare();
-                if (nextColumnOffset >= openColumns.size()) {
+                if (nextColumnOffset >= openColumnCount) {
                     queenSquares.push_back(columnData->getQueenSquare());
+                    columnData->releaseQueenSquare();
                     isSuccess = true;
+std::cout << "SOLVED " << nextColumnOffset << "/" << openColumnCount << std::endl;
                     break;
                 }
 
@@ -132,18 +133,27 @@ std::cout<<nextColumnOffset<<" ("<<queenSquares.size()<< ")"<<std::endl<<std::fl
                 queenSquares.push_back(columnData->getQueenSquare());
             }
         } while (!isSuccess && columnData->nextRow());
+
         if (!isSuccess) {
             queenSquares.pop_back();
+            delete columnData;
+            if (!solveStack.empty()) {
+                columnData = solveStack.front();
+                solveStack.pop_front();
+            }
         } else {
             columnData->releaseQueenSquare();
-        }
-        delete columnData;
-        if (!solveStack.empty()) {
-            columnData = solveStack.front();
-            solveStack.pop_front();
+            delete columnData;
+            break;
         }
     } while (!solveStack.empty());
 
+    while (!solveStack.empty()) {
+        columnData = solveStack.front();
+        solveStack.pop_front();
+        delete columnData;
+    }
+    std::cout << "Final queensqares size:"<< queenSquares.size() <<std::endl;
     return isSuccess;
 }
 
@@ -175,16 +185,23 @@ std::string solveNQueens(int n, std::pair<int, int> mandatoryQueenCoordinates) {
     std::list<square *> queenSquares;
     queenSquares.push_front(new square(mandatoryQueenCoordinates.first, mandatoryQueenCoordinates.second));
 
-    std::vector<int> openColumns;
+    std::list<int> columnCollector;
     for (int column = 0; column < n; column++) {
         if (column != mandatoryQueenCoordinates.first) {
-            openColumns.push_back(column);
+            columnCollector.push_back(column);
         }
+    }
+
+    int *openColumns = new int[columnCollector.size()];
+    int offset = 0;
+    for (int const &column : columnCollector) {
+        openColumns[offset++] = column;
     }
 
     std::string *returnValue;
 //totalRecursions = 0;
-    if (solveColumn(n, queenSquares, openColumns)) {
+std::cout << "START SOLVING " <<  n << std::endl;
+    if (solveColumn(n, queenSquares, openColumns, columnCollector.size())) {
         std::string solution = createPrintableRepresentationOfBoard(n, queenSquares);
         returnValue = new std::string(solution);
     } else {
@@ -194,6 +211,8 @@ std::string solveNQueens(int n, std::pair<int, int> mandatoryQueenCoordinates) {
     for (const auto node: queenSquares)
         delete node;
     queenSquares.clear();
+
+    delete openColumns;
     return *returnValue;
 }
 
